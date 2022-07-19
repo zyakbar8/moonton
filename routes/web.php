f1eb5fd3f0b88887246da90bed73ba171d9468fc
zyakbar8 <zhietan93@gmail.com>
2022-07-19 22:16:41 +0700

    feat: Slicing Sign in and sign up

--- routes/web.php
+++ routes/web.php
@@ -15,25 +15,26 @@ use Inertia\Inertia;
 |
 */
 
-Route::get('admin', function () {
-    return 'Hi Admin';
-})->middleware('role:admin');
-
-Route::get('user', function () {
-    return 'Hi User';
-})->middleware('role:user');
-
-Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
-});
+Route::redirect('/', '/prototype/login');
 
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::prefix('prototype')->name('prototype.')->group(function () {
+    route::get('/login', function () {
+        return Inertia::render('Prototype/Login');
+    })->name('login');
+
+    route::get('/register', function () {
+        return Inertia::render('Prototype/Register');
+    })->name('register');
+
+    route::get('/dashboard', function () {
+        return Inertia::render('Prototype/Dashboard');
+    })->name('dashboard');
+});
+
+
 
 require __DIR__.'/auth.php';
