7d279067134f288e659bee483e1b8798b8e5313b
zyakbar8 <zhietan93@gmail.com>
2022-07-30 12:44:28 +0700

    feat: integration dashboard

--- database/seeders/MovieTableSeeder.php
+++ database/seeders/MovieTableSeeder.php
@@ -22,7 +22,7 @@ class MovieTableSeeder extends Seeder
                 'category' => 'Drama',
                 'video_url' => 'https://www.youtube.com/watch?v=6hB3S9bIaco',
                 'thumbnail' => 'https://m.media-amazon.com/images/M/MV5BNTYxOTYyMzE3NV5BMl5BanBnXkFtZTcwOTMxNDY3Mw@@._V1_.jpg',
-                'rating' => 9.3,
+                'rating' => 4.3,
                 'is_featured' => 1,
             ],
             [
@@ -31,7 +31,7 @@ class MovieTableSeeder extends Seeder
                 'category' => 'Drama',
                 'video_url' => 'https://www.youtube.com/watch?v=UaVTIH8mujA',
                 'thumbnail' => 'https://asset.kompas.com/crops/m2nSp8X-g8zaAsOHGWLJrTWCt24=/266x132:2570x1668/750x500/data/photo/2020/10/13/5f8580f90fab7.jpg',
-                'rating' => 9.2,
+                'rating' => 4.2,
                 'is_featured' => 0,
             ],
             [
@@ -40,7 +40,7 @@ class MovieTableSeeder extends Seeder
                 'category' => 'Drama',
                 'video_url' => 'https://www.youtube.com/watch?v=9O1Iy9od7-A',
                 'thumbnail' => 'https://m.media-amazon.com/images/M/MV5BMWMwMGQzZTItY2JlNC00OWZiLWIyMDctNDk2ZDQ2YjRjMWQ0XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_.jpg',
-                'rating' => 9.0,
+                'rating' => 4.0,
                 'is_featured' => 0,
             ],
         ];
