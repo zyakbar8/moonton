667f9e135259e38d762149d79bda0b152eacc5a0
zyakbar8 <zhietan93@gmail.com>
2022-08-06 16:20:51 +0700

    feat: delete and restore movie

--- routes/web.php
+++ routes/web.php
@@ -31,6 +31,7 @@ Route::middleware(['auth', 'role:user'])->prefix('dashboard')->name('user.dashbo
 });
 
 Route::middleware(['auth', 'role:admin'])->prefix('admin')->name('admin.dashboard.')->group(function () {
+    Route::put('movie/{movie}/resotre', [AdminMovieController::class, 'restore'])->name('movie.restore');
     Route::resource('movie', AdminMovieController::class);
 });
 
